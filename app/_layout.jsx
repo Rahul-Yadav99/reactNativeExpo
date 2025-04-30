576c6d705e2490341fda6de8e41c5a3c26b0ffed
rahul <rahul352sj@gmail.com>
2025-04-30 19:19:51 +0530

    same commit everyTime

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -2,30 +2,12 @@ import React from 'react'
 import { Tabs, useNavigation } from 'expo-router'
 import { FontAwesome } from '@expo/vector-icons'
 import '../global.css'
+import Home from './index'
 const RootLayout = () => {
     return (
-        <Tabs>
-            <Tabs.Screen
-                name='index'
-                options={
-                    {
-                        headerShown: false,
-                        title: 'Home',
-                        tabBarIcon: ({ color, size }) => (<FontAwesome size={size} name='home' color={color} />)
-                    }
-                }
-            />
-            <Tabs.Screen
-                name='about'
-                options={
-                    {
-                        headerShown: false,
-                        title: 'About',
-                        tabBarIcon: ({ color, size }) => (<FontAwesome size={size} name='user' color={color} />)
-                    }
-                }
-            />
-        </Tabs>
+        <>
+            <Home />
+        </>
     )
 }
 
--- app/index.jsx
+++ app/index.jsx
@@ -1,21 +1,59 @@
-import { Pressable, Text, TouchableOpacity, View } from 'react-native'
-import React from 'react'
-import { Link, useRouter } from 'expo-router'
+import { View, Text, TextInput, TouchableOpacity, FlatList, ScrollView, Alert } from 'react-native'
+import React, { useState } from 'react'
+
 const Home = () => {
-    const router = useRouter()
-    const redirect = () => {
-        router.push('/profile')
+    const [formValue, setFormValue] = useState('')
+    const [todoList, setTodoList] = useState([])
+    const addTodo = () => {
+        if (formValue.trim() === '') return
+        setTodoList([...todoList, { id: Date.now().toString(), text: formValue }])
+        setFormValue('')
+    }
+    const deleteTodo = (id) => {
+        setTodoList(todoList.filter((item) => item.id !== id))
     }
     return (
-        <View className='flex-1 bg-yellow-100 items-center justify-center'>
-            <TouchableOpacity activeOpacity={0.8} onPress={redirect} className='bg-fuchsia-500 px-5 py-2 rounded-lg'>
-                <Text className='text-white font-semibold text-2xl'>Redirect</Text>
-            </TouchableOpacity>
-            <Link href={'/about'} asChild>
-                <TouchableOpacity activeOpacity={0.8} className='bg-pink-500 p-3 mt-4 rounded-lg'>
-                    <Text className='text-white font-semibold'>Go to About page.</Text>
+        <View className='flex-1 bg-slate-300 p-6'>
+            <Text className='text-3xl text-gray-700 text-center font-semibold'>📄TODO APP</Text>
+            <View className='flex-row items-center justify-center gap-2 mt-4'>
+                <TextInput
+                    className='border border-gray-500 rounded-lg px-4 bg-white flex-1'
+                    placeholder='Enter your TODO.'
+                    value={formValue}
+                    onChangeText={setFormValue}
+                />
+                <TouchableOpacity
+                    activeOpacity={0.8}
+                    className='bg-green-600 px-6 py-3 rounded-lg justify-center'
+                    onPress={addTodo}
+                >
+                    <Text className='text-green-100'>Add</Text>
                 </TouchableOpacity>
-            </Link>
+            </View>
+            <ScrollView
+                className='mt-2'
+                showsVerticalScrollIndicator={false}
+            >
+                {
+                    todoList.map((item, index) => (
+                        <View
+                            key={index}
+                            className='bg-white mb-2 rounded-lg pl-4'
+                        >
+                            <View className='flex-row items-center justify-between'>
+                                <Text>{item.text}</Text>
+                                <TouchableOpacity
+                                    className='p-4'
+                                    activeOpacity={0.8}
+                                    onPress={() => deleteTodo(item.id)}
+                                >
+                                    <Text>❌</Text>
+                                </TouchableOpacity>
+                            </View>
+                        </View>
+                    ))
+                }
+            </ScrollView>
         </View>
     )
 }
