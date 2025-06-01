8211a2508ec367e35999531eab2f1aae1fe16857
rahul <rahul352sj@gmail.com>
2025-06-01 15:31:14 +0530

    same commit every day

--- app/second/nested.jsx
+++ app/second/nested.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 const ThirdScreen = () => {
     return (
         <View>
-            <Text>nested Screen.</Text>
+            <Text>nested Screen</Text>
         </View>
     )
 }
